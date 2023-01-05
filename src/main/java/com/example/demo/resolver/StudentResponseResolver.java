4a919b2084b27d46a85e92f82ef12de9d381f8b0
Subin Paul <subin.paul@metropolitan.co.za>
2023-01-05 14:55:38 +0530

    Filter to handle All values

--- src/main/java/com/example/demo/resolver/StudentResponseResolver.java
+++ src/main/java/com/example/demo/resolver/StudentResponseResolver.java
@@ -2,6 +2,7 @@ package com.example.demo.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.example.demo.entity.Subject;
+import com.example.demo.enums.SubjectNameFilter;
 import com.example.demo.response.StudentResponse;
 import com.example.demo.response.SubjectResponse;
 import java.util.ArrayList;
@@ -11,13 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentResponseResolver implements GraphQLResolver<StudentResponse> {
 
-  public List<SubjectResponse> getLearningSubjects(StudentResponse studentResponse) {
+  public List<SubjectResponse> getLearningSubjects (StudentResponse studentResponse,
+      SubjectNameFilter subjectNameFilter) {
 
     List<SubjectResponse> learningSubjects = new ArrayList<SubjectResponse>();
 
     if (studentResponse.getStudent().getLearningSubjects() != null) {
       for (Subject subject : studentResponse.getStudent().getLearningSubjects()) {
-        learningSubjects.add(new SubjectResponse(subject));
+        if (subjectNameFilter.name().equalsIgnoreCase("ALL")) {
+          learningSubjects.add(new SubjectResponse(subject));
+        } else if (subjectNameFilter.name().equalsIgnoreCase(subject.getSubjectName())) {
+          learningSubjects.add(new SubjectResponse(subject));
+        }
       }
     }
 
