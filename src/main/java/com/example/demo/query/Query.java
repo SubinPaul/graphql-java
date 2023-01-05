aadd3e968046b12500f8815ccff051e7d533081e
Subin Paul <subin.paul@metropolitan.co.za>
2023-01-05 07:04:48 +0530

    Initial Spring Boot Graph Ql - query

---
+++ src/main/java/com/example/demo/query/Query.java
@@ -0,0 +1,15 @@
+package com.example.demo.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+  public String firstQuery () {
+    return "First Query";
+  }
+
+  public String secondQuery () {
+    return "Second Query";
+  }
+}
